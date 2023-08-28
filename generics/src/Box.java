55ddb13a0575555b0a5e7058feab971ecab863ae
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-08-28 19:58:25 -0300

    Add files via upload

---
+++ generics/src/Box.java
@@ -0,0 +1,8 @@
+//antes do java 7 > Sem generics
+
+public class Box {
+    private Object object;
+
+    public void set(Object object) { this.object = object; }
+    public Object get() { return object; }
+}
---
+++ generics/src/Box2.java
@@ -0,0 +1,14 @@
+/**
+Versão genérica da classe Box.
+@param <T> o tipo do valor sendo armazenado
+*/
+
+public class Box2 {
+
+	// T representa "Type" (tipo)
+    private T t;
+
+    public void set(T t) { this.t = t; }
+    public T get() { return t; }
+}
+}
